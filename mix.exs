92d4d80f03cdca56e0a0f74e6c417b61f941fa70
Eric Saxby <sax@livinginthepast.org>
2024-05-26 10:01:28 -0700

    Add ex_doc

--- mix.exs
+++ mix.exs
@@ -19,7 +19,10 @@ defmodule EctoEmail.MixProject do
       app: :ecto_email,
       deps: deps(),
       dialyzer: dialyzer(),
+      docs: docs(),
       elixir: "~> 1.14",
+      homepage_url: "https://github.com/synchronal/ecto_email",
+      source_url: "https://github.com/synchronal/ecto_email",
       start_permanent: Mix.env() == :prod,
       version: "0.1.0"
     ]
@@ -31,6 +34,7 @@ defmodule EctoEmail.MixProject do
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
       {:ecto_temp, "~> 1.1", only: :test},
+      {:ex_doc, "~> 0.33.0", only: :dev, runtime: false},
       {:mix_audit, "~> 2.1", only: [:dev, :test], runtime: false}
     ]
 
@@ -40,5 +44,11 @@ defmodule EctoEmail.MixProject do
       plt_add_deps: :app_tree,
       plt_core_path: "_build/plts/#{Mix.env()}",
       plt_local_path: "_build/plts/#{Mix.env()}"
+    ]
+
+  defp docs,
+    do: [
+      main: "readme",
+      extras: ["README.md", "LICENSE.md"]
     ]
 end
